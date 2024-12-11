1144c7ce962ebcb82a192724f6fa40898fb77592
JackHat1 <panadreas5@gmail.com>
2024-12-11 14:03:09 +0200

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,37 @@
+/*
+ * Project: MT07 CAN Bus Interface
+ * Author: Andreas Panagiotakis
+ * Date: 3/8/2024
+ * License: MIT License
+ * 
+ * Description:
+ * This program uses the MT07_CANBus library to interact with the CAN bus of a Yamaha MT-07 motorcycle (2014 model).
+ * It initializes the CAN bus, decodes incoming messages, and displays the following data in real-time:
+ * - Gear position
+ * - Throttle position sensor (TPS)
+ * - Motor and air temperature
+ * - Speed and RPM
+ * 
+ * Usage:
+ * - Connect the MCP2515 module to the ESP32.
+ * - Wire the CAN bus to the motorcycle.
+ * - Compile and upload the program to the ESP32.
+ * - View the decoded data in the Serial Monitor at 115200 baud.
+ * 
+ * License Information:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "MT07_CANBus.h"
 
 const int CS_PIN = 15;  // Chip select pin for MCP2515
