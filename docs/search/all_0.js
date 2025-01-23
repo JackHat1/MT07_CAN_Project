9af98c4206554bd4ac336c8fdf90ac2bd044b703
Andreas Panagiotakis <panadreas5@gmail.com>
2025-01-23 03:12:00 +0200

    Delete docs directory

--- docs/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cs_5fpin_0',['CS_PIN',['../main_8ino.html#ad868ab432809f8b4064fa78838b55675',1,'main.ino']]]
-];
--- docs/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['main_2eino_0',['main.ino',['../main_8ino.html',1,'']]],
-  ['mt07_5fcanbus_2ecpp_1',['MT07_CANBus.cpp',['../_m_t07___c_a_n_bus_8cpp.html',1,'']]]
-];
